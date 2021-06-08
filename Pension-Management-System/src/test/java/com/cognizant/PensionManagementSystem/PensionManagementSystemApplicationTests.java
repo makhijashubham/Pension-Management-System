8fa9a02adc4aeb455ca6ffa9b8122a49e70bb51d
makhijashubham <61048964+makhijashubham@users.noreply.github.com>
2021-06-08 20:50:17 +0530

    Commit

---
+++ Pension-Management-System/src/test/java/com/cognizant/PensionManagementSystem/PensionManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.PensionManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PensionManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
